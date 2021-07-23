72bd96b07a09bd9b61260ba1a4c1a05720d767a5
caio <caio.mateus.r@gmail.com>
2021-07-23 17:30:12 -0300

    Projeto Iniciado, Data criado

---
+++ Data/Models/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    class Produto : BaseModels
+    {
+        public string Nome { get; set; }
+
+        public Produto(int id, string nome)
+        {
+            this.Id = id;
+            this.Nome = nome;
+
+        }
+
+        public Produto()
+        {
+
+        }
+    }
+
+}
